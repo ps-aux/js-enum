6b6bd94eb35fc38326b8df42f8ac7f54bce03970
Lukas <seriouz.accz@gmail.com>
2017-08-13 00:14:54 +0200

    Add ya-logger and link .babelrc

---
+++ packages/ya-logger/src/Logger.js
@@ -0,0 +1,42 @@
+const CONSOLE_MAPPING = {
+    trace: 'debug'
+}
+
+const objToMsg = obj => {
+    // JSON.strinfigy might be needed sometimes
+    // for example with Raven.js
+    return obj
+}
+
+class Logger {
+
+    log = (level, msg, data) => {
+        // Try to remap
+        level = CONSOLE_MAPPING[level] || level
+        const logger = console[level] || console.info
+
+        const args = [msg]
+        if (data)
+            args.push(objToMsg(data))
+
+        logger(...args)
+    }
+
+    info = (msg, data) => this.log('info', msg, data)
+
+    debug = (msg, data) => this.log('debug', msg, data)
+
+    trace = (msg, data) => this.log('trace', msg, data)
+
+    error = (...args) => {
+        let toBeLogged = 'An error occurred'
+        console.debug(args.length)
+        if (args.length > 0) {
+            console.error.apply(console, args)
+        } else {
+            console.error(toBeLogged)
+        }
+    }
+}
+
+export default new Logger()
---
+++ packages/ya-logger/webpack.config.js
@@ -0,0 +1,53 @@
+/*
+* Taken from https://github.com/krasimir/webpack-library-starter
+*/
+
+const webpack = require('webpack')
+const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin
+const path = require('path')
+const env = require('yargs').argv.env // use --env with webpack 2
+
+const libraryName = require('./package.json').name
+
+const plugins = []
+let outputFile = null
+
+if (env === 'build') {
+    plugins.push(new UglifyJsPlugin({minimize: true}))
+    outputFile = libraryName + '.min.js'
+} else {
+    outputFile = libraryName + '.js'
+}
+
+const config = {
+    entry: path.resolve(__dirname, 'src/Logger.js'),
+    devtool: 'source-map',
+    output: {
+        path: path.resolve(__dirname, 'lib'),
+        filename: outputFile,
+        library: libraryName,
+        libraryTarget: 'umd',
+        umdNamedDefine: true
+    },
+    module: {
+        rules: [
+            {
+                test: /(\.jsx|\.js)$/,
+                loader: 'babel-loader',
+                exclude: /(node_modules)/
+            },
+            {
+                test: /(\.jsx|\.js)$/,
+                loader: 'eslint-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        modules: [path.resolve('./node_modules'), path.resolve('.')],
+        extensions: ['.json', '.js']
+    },
+    plugins: plugins
+}
+
+module.exports = config
